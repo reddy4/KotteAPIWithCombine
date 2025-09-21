56c9bd8ed81388cc1647c10951365550a1ae7120
Reddanna Kotte <reddy.iphonedev@gmail.com>
2025-09-21 14:53:37 +0530

    Access Modifier updated

--- Sources/KotteAPIWithCombine/GenericAPI.swift
+++ Sources/KotteAPIWithCombine/GenericAPI.swift
@@ -12,7 +12,7 @@ public class GenericAPI {
     
     public init() { }
     
-    let shared = GenericAPI()
+    public let shared = GenericAPI()
     func getData<T: Decodable>(from url: String)->AnyPublisher<[T], Error> {
         URLSession.shared.dataTaskPublisher(for: URL(string: url)!)
             .map(\.data)

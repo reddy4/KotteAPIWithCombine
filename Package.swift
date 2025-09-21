cf82cbc54441494c0e6b5ca542c9a3f16df8e9d7
Reddanna Kotte <reddy.iphonedev@gmail.com>
2025-09-21 14:47:19 +0530

    spaces

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "KotteAPIWithCombine",
-    platforms: [.iOS(.v15),.macOS(.v14), .visionOS(.v1)],
+    platforms: [.iOS(.v15), .macOS(.v14), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
